298ebfc2181359a9ccb59e18c73a30069262ec87
khtwou123 <khtwou1234@gmail.com>
2025-02-25 14:57:11 +0800

    更新: 將 Employee.java 由 javax.persistence 換成 jakarta.persistence

--- springbootdb/src/main/java/com/example/springbootdb/model/Employee.java
+++ springbootdb/src/main/java/com/example/springbootdb/model/Employee.java
@@ -1,7 +1,7 @@
 package com.example.springbootdb.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
@@ -25,23 +25,18 @@ public class Employee {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getDepartment() {
         return department;
     }
-
     public void setDepartment(String department) {
         this.department = department;
     }
